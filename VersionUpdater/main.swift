2de0ed37cb3a0c328e9c84fc7a1921a0c857250b
Zero <zero.griffin@gmail.com>
2018-06-30 01:18:34 +0200

    Initial Commit

---
+++ VersionUpdater/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  VersionUpdater
+//
+//  Created by zero on 30.06.18.
+//  Copyright © 2018 iturbide. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
