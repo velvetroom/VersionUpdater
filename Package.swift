db22ec27f028c00f6f96bc4dfc1e3f4c89c63e97
Zero <zero.griffin@gmail.com>
2018-06-30 03:52:45 +0200

    Remove swift package manager

--- Package.swift
+++
@@ -1,8 +0,0 @@
-// swift-tools-version:4.0
-import PackageDescription
-
-let package:Package = Package(
-    name:"VersionUpdater",
-    dependencies:[.package(url:"https://github.com/velvetroom/SwiftToShell.git", .branch("master"))],
-    targets:[.target(name:"VersionUpdater", dependencies:["SwiftToShell"], path:"Source/")]
-)
